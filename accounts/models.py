b19cd42c3757c8e4489704a05b3fb7d13e794744
gautamk.inexture <gautamk.inexture@gmail.com>
2022-08-02 15:06:59 +0530

    authentication part

---
+++ accounts/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, password2=None, **other_fields):
+        """
+        Creates and saves a User with the given email,password and extra fields which are given User model.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **other_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, password2=None, **other_fields):
+        """
+        Creates and saves a superuser with the given email,phone_no(required fields),and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **other_fields
+
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+# Create your models here.
+class User(AbstractBaseUser):
+    """
+    Use of AbstractBaseUser for customize User as per our requirements.
+    """
+    email = models.EmailField(
+        verbose_name='Email',
+        max_length=255,
+        unique=True,
+    )
+    '''
+    adding custom fields if we need.
+    '''
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_no = PhoneNumberField(null=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'  # this field is for login purpose
+    REQUIRED_FIELDS = ['phone_no']
+
+    def __str__(self):
+        return self.first_name
+
+    def has_perm(self, perm, obj=None):
+        """Does the user have a specific permission?"""
+        # Simplest possible answer: Yes, always
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        """Does the user have permissions to view the app `app_label`?"""
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        # "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+
---
+++ accounts/views.py
@@ -0,0 +1,130 @@
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+from rest_framework.views import APIView
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+from .permissions import IsOwner
+from django_rest_passwordreset.views import (ResetPasswordRequestToken, ResetPasswordConfirm,
+                                             ResetPasswordValidateToken)
+from .models import User
+from accounts.serializer import (UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer,
+                                 UserChangePasswordSerializer)
+
+
+# generating custom token
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    """
+    This method is used for generating token. 
+    """
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+
+# Create your views here.
+
+
+class UserRegistration(APIView):
+    """
+    User Registration view.
+    """
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            token = get_tokens_for_user(user)
+            return Response({"token": token, "msg": "done"}, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogin(APIView):
+    """
+    User login view.
+    """
+    permission_classes = [AllowAny]
+
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user:
+                token = get_tokens_for_user(user)
+                return Response({'token': token, 'msg': 'Login Success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': {'non_field_errors': ['Email or password is not valid']}},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserChangePasswordView(APIView):
+    """
+    View for change password
+    """
+
+    permission_classes = [IsAuthenticated]
+    """
+    IsAuthenticated class used for verifying that user is must for this specified operation
+    """
+
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(data=request.data, context={'user': request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg': 'Password changed successfully'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfile(viewsets.ModelViewSet):
+    """
+    This is userprofile class in which we use ModelViewset. In this class we use different-different methods
+    as per our requirement.
+    """
+    permission_classes = [IsAdminUser]
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+
+
+"""
+Overriding of ResetPassword class to customize message part".
+
+ResetPassword class works in three steps(token passing, token validation, token confirmation)
+"""
+
+
+class ResetPasswordConfirmView(ResetPasswordConfirm):
+    """
+    this is a customized part of ResetPasswordConfirm class.
+    """
+
+    def post(self, request, *args, **kwargs):
+        super(ResetPasswordConfirmView, self).post(request, *args, **kwargs)
+        return Response({'status': 'OK', 'message': ' Password Reset Successfully'})
+
+
+class ResetPasswordValidateView(ResetPasswordValidateToken):
+    """
+    this is a customized part of ResetPasswordValidateToken class.
+    """
+
+    def post(self, request, *args, **kwargs):
+        super(ResetPasswordValidateView, self).post(request, *args, **kwargs)
+        return Response({'status': 'OK', 'message': ' Token validation succeed'})
+
+
+class ResetPasswordTokenView(ResetPasswordRequestToken):
+    """
+    this is a customized part of ResetPasswordRequestToken class.
+    """
+
+    def post(self, request, *args, **kwargs):
+        super(ResetPasswordTokenView, self).post(request, *args, **kwargs)
+        return Response({'status': 'OK', 'message': ' Token Passed successfully'})
+
+
+
